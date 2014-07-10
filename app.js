521f0bf69250697e544cd3e4f84c4bb544100aa8
Teemu Heikkilä <teemu.heikkila@emblica.org>
2014-07-10 15:41:02 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Dependencies and globals
+
+var path 			= require('path');
+var express         = require('express');
+var bodyParser      = require('body-parser');
+
+var session    		= require('express-session');
+var MongoStore 		= require('connect-mongo')(session);
+
+
+var app     		= express();
+
+var config 			= require('./config');
+
+app.use(session({
+	secret: config.session_secret,
+	store: new MongoStore({
+		db : config.mongo.db,
+		host: config.mongo.host
+	})
+}));
+
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// Use JSONP because of cross origin requests
+app.set("jsonp callback", true);
+
+
+
+app.listen(config.port);
+console.log("App running on", config.port);
+
---
+++ config.js
@@ -0,0 +1,11 @@
+// Config for comments
+
+module.exports = {
+	port: 4000,
+	session_secret: "add the secret",
+	mongo: {
+		host:'127.0.0.1',
+		db: 'commentsapp'
+		}
+
+}
